26b51e777ebef9e10377046567974590ccd7768b
dengchao-ryan <ryanchao0425@foxmail.com>
2018-03-23 22:55:53 +0800

    修改账号信息dao

--- yuesuke/src/main/java/com/dao/AccountDao.java
+++ yuesuke/src/main/java/com/dao/AccountDao.java
@@ -3,23 +3,24 @@ package com.dao;/***************************************************************
  * @Project: yuesuke
  * @Package com.dao
  * @author dengchao
- * @date 2018/3/20 22:29
+ * @date 2018/3/20 22:44
  * @Copyright: 2018 www.zyht.com Inc. All rights reserved.
  * @version V1.0
  */
 
 import com.domain.Account;
+import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author dengchao
- * @InterfaceName AccountDao
- * @Description 学号/账号 dao接口
+ * @ClassName AccountDaoImpl
+ * @Description 学号/账号类dao实现类
  * @date 2018/3/20
  */
-public interface AccountDao {
+public class AccountDao extends BaseDao<Account> implements Dao<Account>{
     /**
      * @Title: deleteById
      * @Description: 根据id删除数据
@@ -28,7 +29,10 @@ public interface AccountDao {
      * @return 返回受影响行数
      * @date 2018/3/20
      */
-    public Integer deleteById(Long id);
+    @Override
+    public Integer deleteById(Long id) {
+        return super.sqlSessionTemplate.delete(getMybatisNameSpace()+"deleteById",id);
+    }
     /**
      * @Title: deleteByIds
      * @Description: 根据多个id删除多个
@@ -37,7 +41,10 @@ public interface AccountDao {
      * @return 返回受影响行数
      * @date 2018/3/20
      */
-    public Integer deleteByIds(Long[] ids);
+    @Override
+    public Integer deleteByIds(Long[] ids) {
+        return super.sqlSessionTemplate.delete(getMybatisNameSpace()+"deleteByIds",ids);
+    }
     /**
      * @Title: save
      * @Description: 保存/添加
@@ -46,7 +53,10 @@ public interface AccountDao {
      * @return 返回受影响行数
      * @date 2018/3/20
      */
-    public Integer save(Account account);
+    @Override
+    public Integer save(Account account) {
+        return super.sqlSessionTemplate.insert(getMybatisNameSpace()+"save",account);
+    }
     /**
      * @Title: update
      * @Description: 修改/更新
@@ -55,7 +65,10 @@ public interface AccountDao {
      * @return 返回受影响行数
      * @date 2018/3/20
      */
-    public Integer update(Account account);
+    @Override
+    public Integer update(Account account) {
+        return super.sqlSessionTemplate.update(getMybatisNameSpace()+"update",account);
+    }
     /**
      * @Title: findById
      * @Description: 根据id查找
@@ -64,7 +77,10 @@ public interface AccountDao {
      * @return 返回查询结果对象
      * @date 2018/3/20
      */
-    public Account findById(Long id);
+    @Override
+    public Account findById(Long id) {
+        return super.sqlSessionTemplate.selectOne(getMybatisNameSpace()+"findById",id);
+    }
     /**
      * @Title: findByCondition
      * @Description: 根据条件查询
@@ -73,7 +89,10 @@ public interface AccountDao {
      * @return 返回查询结果对象list集合
      * @date 2018/3/20
      */
-    public List<Account> findByCondition(Map<String,Object> map);
+    @Override
+    public List<Account> findByCondition(Map<String, Object> map) {
+        return super.sqlSessionTemplate.selectList(getMybatisNameSpace()+"findByCondition",map);
+    }
     /**
      * @Title: findByConditionForPage
      * @Description: 根据条件分页查询
@@ -84,5 +103,9 @@ public interface AccountDao {
      * @return 返回查询结果list集合
      * @date 2018/3/20
      */
-    public List<Account> findByConditionForPage(Map<String,Object> map,Integer start,Integer size);
+    @Override
+    public List<Account> findByConditionForPage(Map<String, Object> map, Integer start, Integer size) {
+        RowBounds rowBounds=new RowBounds(start,size);
+        return super.sqlSessionTemplate.selectList(getMybatisNameSpace()+"findByConditionForPage",map,rowBounds);
+    }
 }
